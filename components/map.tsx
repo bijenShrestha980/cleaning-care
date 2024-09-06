bed3bd2cc1e4816e622b4294c532277f6fb44a66
bijenShrestha980 <s.bijen93@gmail.com>
2024-09-06 17:41:55 +0545

    fix: map remove

--- components/map.tsx
+++ components/map.tsx
@@ -1,41 +1,41 @@
-"use client";
-import ReactMapboxGl, {
-  Layer,
-  Feature,
-  ZoomControl,
-  ScaleControl,
-  Marker,
-} from "react-mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-import { user_1 } from "@/constants/images";
-import Image from "next/image";
+// "use client";
+// import ReactMapboxGl, {
+//   Layer,
+//   Feature,
+//   ZoomControl,
+//   ScaleControl,
+//   Marker,
+// } from "react-mapbox-gl";
+// import "mapbox-gl/dist/mapbox-gl.css";
+// import { user_1 } from "@/constants/images";
+// import Image from "next/image";
 
-const Map = ReactMapboxGl({
-  accessToken: process.env.map || "",
-});
+// const Map = ReactMapboxGl({
+//   accessToken: process.env.map || "",
+// });
 
-const CustomMap = ({ width = "100vw", height = "100vh" }) => {
-  return (
-    <Map
-      style="mapbox://styles/bijen/cl4887nzz003u15p1s53vhbhy"
-      containerStyle={{
-        height,
-        width,
-      }}
-      center={[85.352, 27.721]}
-      zoom={[13]}
-    >
-      <Layer type="symbol" id="marker" layout={{ "icon-image": "harbor-15" }}>
-        <Feature coordinates={[85.352, 27.721]} />
-      </Layer>
-      <Marker coordinates={[-0.2416815, 51.5285582]} anchor="bottom">
-        <Image src={user_1} alt="user" width={32} height={32} />
-      </Marker>
-      <ZoomControl />
+// const CustomMap = ({ width = "100vw", height = "100vh" }) => {
+//   return (
+//     <Map
+//       style="mapbox://styles/bijen/cl4887nzz003u15p1s53vhbhy"
+//       containerStyle={{
+//         height,
+//         width,
+//       }}
+//       center={[85.352, 27.721]}
+//       zoom={[13]}
+//     >
+//       <Layer type="symbol" id="marker" layout={{ "icon-image": "harbor-15" }}>
+//         <Feature coordinates={[85.352, 27.721]} />
+//       </Layer>
+//       <Marker coordinates={[-0.2416815, 51.5285582]} anchor="bottom">
+//         <Image src={user_1} alt="user" width={32} height={32} />
+//       </Marker>
+//       <ZoomControl />
 
-      <ScaleControl />
-    </Map>
-  );
-};
+//       <ScaleControl />
+//     </Map>
+//   );
+// };
 
-export default CustomMap;
+// export default CustomMap;
