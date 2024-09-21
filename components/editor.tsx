e100fa3a225a71f6b7d80bc84dcf26bb06d30ca0
bijenShrestha980 <s.bijen93@gmail.com>
2024-09-21 13:24:09 +0545

    fix: remove editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,71 +1,71 @@
-"use client";
-import ReactQuill from "react-quill";
+// "use client";
+// import ReactQuill from "react-quill";
 
-function CustomEditor(props: React.ComponentProps<typeof ReactQuill>) {
-  const modules = {
-    toolbar: [
-      [
-        {
-          font: ["sans", "serif", "monospace"],
-        },
-        { header: [1, 2, false] },
-      ],
-      ["bold", "italic", "underline", "strike"],
-      [
-        {
-          color: [
-            "#FFFFFF",
-            "#000000",
-            "#e60000",
-            "#ff9900",
-            "#ffff00",
-            "#008a00",
-            "#0066cc",
-            "#9933ff",
-          ],
-        },
-        {
-          background: [
-            "#FFFFFF",
-            "#000000",
-            "#e60000",
-            "#ff9900",
-            "#ffff00",
-            "#008a00",
-            "#0066cc",
-            "#9933ff",
-          ],
-        },
-      ],
-      [{ script: "sub" }, { script: "super" }],
-      [
-        { header: 1 },
-        {
-          header: 2,
-        },
-        "blockquote",
-        "code-block",
-      ],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
-      ["direction", "align"],
-      ["link", "image", "video", "formula"],
-      ["clean"],
-    ],
-  };
+// function CustomEditor(props: React.ComponentProps<typeof ReactQuill>) {
+//   const modules = {
+//     toolbar: [
+//       [
+//         {
+//           font: ["sans", "serif", "monospace"],
+//         },
+//         { header: [1, 2, false] },
+//       ],
+//       ["bold", "italic", "underline", "strike"],
+//       [
+//         {
+//           color: [
+//             "#FFFFFF",
+//             "#000000",
+//             "#e60000",
+//             "#ff9900",
+//             "#ffff00",
+//             "#008a00",
+//             "#0066cc",
+//             "#9933ff",
+//           ],
+//         },
+//         {
+//           background: [
+//             "#FFFFFF",
+//             "#000000",
+//             "#e60000",
+//             "#ff9900",
+//             "#ffff00",
+//             "#008a00",
+//             "#0066cc",
+//             "#9933ff",
+//           ],
+//         },
+//       ],
+//       [{ script: "sub" }, { script: "super" }],
+//       [
+//         { header: 1 },
+//         {
+//           header: 2,
+//         },
+//         "blockquote",
+//         "code-block",
+//       ],
+//       [
+//         { list: "ordered" },
+//         { list: "bullet" },
+//         { indent: "-1" },
+//         { indent: "+1" },
+//       ],
+//       ["direction", "align"],
+//       ["link", "image", "video", "formula"],
+//       ["clean"],
+//     ],
+//   };
 
-  return (
-    <ReactQuill
-      theme="snow"
-      modules={modules}
-      {...props}
-      className="mb-10 min-h-6"
-    />
-  );
-}
+//   return (
+//     <ReactQuill
+//       theme="snow"
+//       modules={modules}
+//       {...props}
+//       className="mb-10 min-h-6"
+//     />
+//   );
+// }
 
-export default CustomEditor;
+// export default CustomEditor;
