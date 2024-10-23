ed8d21ab35c080d0584174a27fc27afe913a7af3
bijenShrestha980 <s.bijen93@gmail.com>
2024-10-23 23:20:02 +0545

    chore: signin bg color change

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -3,7 +3,6 @@ import { createSession } from "@/lib/session";
 
 export async function POST(request: Request) {
   const res = await request.json();
-  console.log("first", res);
   const session = await createSession(res.data);
   return Response.json({ success: true, session });
 }
