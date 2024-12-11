27c1a53575261ae01d77d53f77a509b5207ca8c2
bijenShrestha980 <s.bijen93@gmail.com>
2024-12-11 14:28:59 +0545

    fix: show all services

--- features/services/components/services-section.tsx
+++ features/services/components/services-section.tsx
@@ -6,28 +6,27 @@ const ServicesSection = async () => {
 
   return (
     <div className="w-full flex flex-wrap justify-center">
-      {services &&
-        services.slice(0, 3).map((service, index) => (
-          <div
-            key={index}
-            className="w-full md:w-[470px] p-2 md:p-4 lg:p-6 flex flex-col items-center gap-4 lg:gap-6"
-          >
-            <CustomImage
-              src={service?.banner_image_url || ""}
-              alt="Residential Cleaning"
-              fill
-              sizes="225px"
-              containerClassName="w-[175px] h-[175px] lg:w-[225px] lg:h-[225px]"
-              className="rounded-full w-full h-full object-cover object-center"
-            />
-            <h5 className="text-primary text-lg md:text-2xl font-medium text-center line-clamp-2">
-              {service.service_name}
-            </h5>
-            <p className="text-base md:text-lg text-primary opacity-50 text-center line-clamp-4">
-              {service.short_description}
-            </p>
-          </div>
-        ))}
+      {services.map((service, index) => (
+        <div
+          key={index}
+          className="w-full md:w-[470px] p-2 md:p-4 lg:p-6 flex flex-col items-center gap-4 lg:gap-6"
+        >
+          <CustomImage
+            src={service?.banner_image_url || ""}
+            alt="Residential Cleaning"
+            fill
+            sizes="225px"
+            containerClassName="w-[175px] h-[175px] lg:w-[225px] lg:h-[225px]"
+            className="rounded-full w-full h-full object-cover object-center"
+          />
+          <h5 className="text-primary text-lg md:text-2xl font-medium text-center line-clamp-2">
+            {service.service_name}
+          </h5>
+          <p className="text-base md:text-lg text-primary opacity-50 text-center line-clamp-4">
+            {service.short_description}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
