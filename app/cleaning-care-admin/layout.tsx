d6a24c08083bfee9d4ebb11f1ef9a3669ae0e9cb
Lokendra Chand <77656792+loken1008@users.noreply.github.com>
2025-01-29 20:29:13 +0000

    Update layout.tsx

--- app/cleaning-care-admin/layout.tsx
+++ app/cleaning-care-admin/layout.tsx
@@ -1,9 +1,9 @@
 import SidebarProvider from "@/providers/sidebar-provider";
 
 export default function RootLayout({
-  children,
+  // children,
 }: {
-  children: React.ReactNode;
+  // children: React.ReactNode;
 }) {
   return <SidebarProvider>{children}</SidebarProvider>;
 }
