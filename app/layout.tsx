96ca45cd8e3e6c10807064c806923b260a769b04
Lokendra Chand <77656792+loken1008@users.noreply.github.com>
2025-01-29 20:44:16 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,17 +43,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <!-- Google Tag Manager -->
-        <Script>
+{/*         Google Tag Manager  */}
+          <Script id="gtm" strategy="afterInteractive">
           {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5XRHMQ22');
-        `}
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5XRHMQ22');
+          `}
         </Script>
-      <!-- End Google Tag Manager -->
+{/*       End Google Tag Manager */}
         <Script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -67,12 +67,12 @@ export default function RootLayout({
       <body
         className={`${fontInter.variable} ${montserratSans.variable} ${montserratItalic.variable} ${bricolageGrotesqueSans.variable} min-h-screen bg-background antialiased`}
       >
-           <!-- Google Tag Manager (noscript) -->
+{/* Google Tag Manager (noscript) */}
     <noscript>
       <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XRHMQ22"
     height="0" width="0" style="display:none;visibility:hidden"></iframe>
     </noscript>
-    <!-- End Google Tag Manager (noscript) -->       
+{/*     End Google Tag Manager (noscript)      */}
   <TanstackProvider>
           <TooltipProvider>{children}</TooltipProvider>
         </TanstackProvider>
