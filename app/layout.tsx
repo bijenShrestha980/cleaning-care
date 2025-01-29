f47da7a5bc29d11c4e08f8104f1f16f0373c1f7d
Lokendra Chand <77656792+loken1008@users.noreply.github.com>
2025-01-29 20:37:39 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,11 +44,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script>
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5XRHMQ22');</script>
+        })(window,document,'script','dataLayer','GTM-5XRHMQ22');
+        `}
+        </script>
       <!-- End Google Tag Manager -->
         <Script id="google-analytics">
           {`
@@ -64,8 +68,11 @@ export default function RootLayout({
         className={`${fontInter.variable} ${montserratSans.variable} ${montserratItalic.variable} ${bricolageGrotesqueSans.variable} min-h-screen bg-background antialiased`}
       >
            <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XRHMQ22"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript>
+      
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XRHMQ22"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
     <!-- End Google Tag Manager (noscript) -->       
   <TanstackProvider>
           <TooltipProvider>{children}</TooltipProvider>
